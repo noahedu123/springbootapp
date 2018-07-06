7f88ab81b4f06fafcaa25507888be3dfbfd828f0
noahedu123 <noahedu123@163.com>
2018-07-06 09:05:37 +0800

    sun

---
+++ src/main/java/com/cn/userenum/UserLoginEnum.java
@@ -0,0 +1,25 @@
+package com.cn.userenum;
+
+import lombok.Getter;
+
+/**
+ * 用户登录状态枚举
+ */
+@Getter
+public enum UserLoginEnum {
+
+    FAIL(0,"用户名或密码错误"),
+    SUCCESS(1,"登录成功"),
+    REGISTERED(2,"用户未注册"),
+    LOCK(3,"用户锁定")
+    ;
+    private Integer code;
+
+    private String message;
+
+    UserLoginEnum(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
+
